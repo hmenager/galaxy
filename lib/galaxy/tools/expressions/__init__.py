c49be76c6aea8291780f90bdee142588be8f0481
John Chilton <jmchilton@gmail.com>
2015-12-13 10:40:26 +0000

    Some libraries for dealing with sandboxed JavaScript expressions.
    Based on work in cwltool.

---
+++ lib/galaxy/tools/expressions/__init__.py
@@ -0,0 +1,8 @@
+from .evaluate import evaluate
+from .script import EXPRESSION_SCRIPT_CALL, write_evalute_script
+
+__all__ = [
+    'evaluate',
+    'EXPRESSION_SCRIPT_CALL',
+    'write_evalute_script',
+]
---
+++ lib/galaxy/tools/expressions/evaluate.py
@@ -0,0 +1,42 @@
+import json
+import os
+import subprocess
+
+from galaxy.tools.deps.commands import which
+FILE_DIRECTORY = os.path.normpath(os.path.dirname(os.path.join(__file__)))
+NODE_ENGINE = os.path.join(FILE_DIRECTORY, "cwlNodeEngine.js")
+
+
+def evaluate(config, input):
+    application = _find_engine(config)
+
+    default_context = {
+        "engineConfig": [],
+        "job": {},
+        "context": None,
+        "outdir": None,
+        "tmpdir": None,
+    }
+
+    new_input = default_context
+    new_input.update(input)
+
+    sp = subprocess.Popen([application, NODE_ENGINE],
+                          shell=False,
+                          close_fds=True,
+                          stdin=subprocess.PIPE,
+                          stdout=subprocess.PIPE)
+
+    (stdoutdata, stderrdata) = sp.communicate(json.dumps(new_input) + "\n\n")
+    if sp.returncode != 0:
+        args = (json.dumps(new_input, indent=4), stdoutdata, stderrdata)
+        message = "Expression engine returned non-zero exit code on evaluation of\n%s%s%s" % args
+        raise Exception(message)
+
+    return json.loads(stdoutdata)
+
+
+def _find_engine(config):
+    """ TODO: Use Python variant of this.
+    """
+    return which("nodejs") or which("node") or "node"
