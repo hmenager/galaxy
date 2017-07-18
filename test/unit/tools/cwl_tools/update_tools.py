4a74ad9a76c4e266b8383eac31f4ecfb7a84b76d
John Chilton <jmchilton@gmail.com>
2017-07-18 10:48:11 -0400

    Workflow TODOs.

--- test/unit/tools/cwl_tools/update_tools.py
+++ test/unit/tools/cwl_tools/update_tools.py
@@ -48,11 +48,33 @@ CWL_FILES = {
         "cat3-tool.cwl",
         "count-lines1-wf.cwl",
         "count-lines2-wf.cwl",
+        "count-lines3-job.json",  # needs whale.txt and hello.txt
+        "count-lines3-wf.cwl",  # ScatterFeatureRequirement - File[] -> int[]
+        "count-lines4-job.json",
+        "count-lines4-wf.cwl",  # ScatterFeatureRequirement, MultipleInputFeatureRequirement (File, File) -> int[]
+        "count-lines5-wf.cwl",  # default input file :(
         "hello.txt",
         "parseInt-tool.cwl",
+        "record-output-job.json",
+        "record-output.cwl",  # record inputs and outputs
+        "ref.fasta",  # Used by record-output-job.json...
+        "scatter-job1.json",
+        "scatter-job2.json",
+        "scatter-wf1.cwl",  # ScatterFeatureRequirement - string[] -> string[]
+        "scatter-wf2.cwl",  # ScatterFeatureRequirement, scatterMethod: nested_crossproduct
+        "scatter-wf3.cwl",  # ScatterFreatureRequirement, scatterMethod: flat_crossproduct , $graph
+        "scatter-wf4.cwl",  # ScatterFreatureRequirement, scatterMethod: dotproduct , $graph
         "wc-tool.cwl",
+        "wc2-tool.cwl",
+        "wc3-tool.cwl",
+        "wc4-tool.cwl",
+        "whale.txt",
     ]
 }
+
+# TODO: v1/
+#     scatter stuff: wc2-tool.cwl, wc3-tool.cwl, wc4-tool.cwl, count-lines3-wf.cwl, count-lines4-wf.cwl, count-lines5-wf.cwl
+#     record stuff:  record-output.cwl
 
 
 for version in CWL_FILES.keys():
