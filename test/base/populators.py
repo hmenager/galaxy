26a27b97506597709b7c5a057dde354c4d0ea285
John Chilton <jmchilton@gmail.com>
2018-04-19 16:06:41 -0400

    [WIP] Dynamic Galaxy tools.
     - Introduce models and a API for creating tools dynamically.
     - Use Galaxy's testing-only YAML based representation of tools to prototype this.
     - Extend Format 2 workflow definitions to allow embedding tools directly into workflows, either directly or using a CWL-style @import syntax.
    
    Testing:
    
    Test cases demonstrating tools can be imported (only by admins) and are runnable are included with this commit. More test cases regarding workflow use of dynamic tools and Format 2 workflow definition extensions are also included.
    
    These tests can be run with the following commands:
    
    ```
    ./run_tests.sh -api test/api/test_tools.py:ToolsTestCase.test_nonadmin_users_cannot_create_tools
    ./run_tests.sh -api test/api/test_tools.py:ToolsTestCase.test_dynamic_tool_1
    ./run_tests.sh -api test/api/test_workflows.py:WorkflowsApiTestCase.test_import_export_dynamic
    ./run_tests.sh -api test/api/test_workflows_from_yaml.py:WorkflowsFromYamlApiTestCase.test_workflow_embed_tool
    ./run_tests.sh -api test/api/test_workflows_from_yaml.py:WorkflowsFromYamlApiTestCase.test_workflow_import_tool
    ```

--- test/base/populators.py
+++ test/base/populators.py
@@ -254,6 +254,16 @@ class BaseDatasetPopulator(object):
     def cancel_job(self, job_id):
         return self._delete("jobs/%s" % job_id)
 
+    def create_tool(self, representation):
+        if isinstance(representation, dict):
+            representation = json.dumps(representation)
+        payload = dict(
+            representation=representation,
+        )
+        create_response = self._post("dynamic_tools", data=payload, admin=True)
+        assert create_response.status_code == 200, create_response
+        return create_response.json()
+
     def _summarize_history(self, history_id):
         pass
 
@@ -500,7 +510,7 @@ class DatasetPopulator(BaseDatasetPopulator):
     def __init__(self, galaxy_interactor):
         self.galaxy_interactor = galaxy_interactor
 
-    def _post(self, route, data=None, files=None):
+    def _post(self, route, data=None, files=None, admin=False):
         if data is None:
             data = {}
 
@@ -508,7 +518,7 @@ class DatasetPopulator(BaseDatasetPopulator):
         if files is not None:
             del data["__files"]
 
-        return self.galaxy_interactor.post(route, data, files=files)
+        return self.galaxy_interactor.post(route, data, files=files, admin=admin)
 
     def _get(self, route, data=None):
         if data is None:
@@ -727,11 +737,11 @@ class WorkflowPopulator(BaseWorkflowPopulator, ImporterGalaxyInterface):
         self.dataset_populator = DatasetPopulator(galaxy_interactor)
         self.dataset_collection_populator = DatasetCollectionPopulator(galaxy_interactor)
 
-    def _post(self, route, data=None):
+    def _post(self, route, data=None, admin=False):
         if data is None:
             data = {}
 
-        return self.galaxy_interactor.post(route, data)
+        return self.galaxy_interactor.post(route, data, admin=admin)
 
     def _get(self, route, data=None):
         if data is None:
@@ -750,6 +760,22 @@ class WorkflowPopulator(BaseWorkflowPopulator, ImporterGalaxyInterface):
         upload_response = self._post("workflows", data=data)
         assert upload_response.status_code == 200, upload_response.content
         return upload_response.json()
+
+    def import_tool(self, tool):
+        """ Import a workflow via POST /api/workflows or
+        comparable interface into Galaxy.
+        """
+        upload_response = self._import_tool_response(tool)
+        assert upload_response.status_code == 200, upload_response
+        return upload_response.json()
+
+    def _import_tool_response(self, tool):
+        tool_str = json.dumps(tool, indent=4)
+        data = {
+            'representation': tool_str
+        }
+        upload_response = self._post("dynamic_tools", data=data, admin=True)
+        return upload_response
 
 
 class LibraryPopulator(object):
