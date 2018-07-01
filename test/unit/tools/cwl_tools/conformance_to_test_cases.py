0ad5e11467bc0a6b304b92b81c50be46fe37e900
Hervé  MENAGER <herve.menager@pasteur.fr>
2018-07-01 15:56:20 -0700

    Add CWL conformance tests in tox and travis

--- test/unit/tools/cwl_tools/conformance_to_test_cases.py
+++ test/unit/tools/cwl_tools/conformance_to_test_cases.py
@@ -29,6 +29,37 @@ ${cwl_test_def}
         self.cwl_populator.run_conformance_test("""${version}""", """${doc}""")
 ''')
 
+GREEN_TESTS = [
+        "170",
+        "cl_gen_arrayofarrays",
+        "directory_output",
+        "docker_json_output_location",
+        "docker_json_output_path",
+        "envvar_req",
+        "expression_any",
+        "expression_any_null",
+        "expression_outputEval",
+        "expression_parseint",
+        "exprtool_directory_literal",
+        "exprtool_file_literal",
+        "initial_workdir_empty_writable",
+        "initial_workdir_empty_writable_docker",
+        "initial_workdir_expr",
+        "initial_workdir_output",
+        "initial_workdir_trailingnl",
+        "initialworkpath_output",
+        "inline_expressions",
+        "metadata",
+        "nameroot_nameext_stdout_expr",
+        "null_missing_params",
+        "rename",
+        "stdinout_redirect",
+        "stdinout_redirect_docker",
+        "stdout_redirect_docker",
+        "stdout_redirect_mediumcut_docker",
+        "stdout_redirect_shortcut_docker",
+        "writable_stagedfiles",
+    ]
 
 def main():
     version = "v1.0"
@@ -40,6 +71,7 @@ def main():
         conformance_tests = yaml.load(f)
 
     tests = ""
+    green_tests = ""
     for i, conformance_test in enumerate(conformance_tests):
         test_with_doc = conformance_test.copy()
         del test_with_doc["doc"]
@@ -53,14 +85,29 @@ def main():
             'cwl_test_def': cwl_test_def,
             'label': label,
         })
+        if label in GREEN_TESTS:
+            green_tests = green_tests + TEST_TEMPLATE.safe_substitute({
+                'version_simple': version_simple,
+                'version': version,
+                'doc': conformance_test['doc'],
+                'cwl_test_def': cwl_test_def,
+                'label': label,
+            })
 
     test_file_contents = TEST_FILE_TEMPLATE.safe_substitute({
         'version_simple': version_simple,
         'tests': tests
     })
 
+    green_test_file_contents = TEST_FILE_TEMPLATE.safe_substitute({
+        'version_simple': version_simple,
+        'tests': green_tests
+    })
+
     with open(os.path.join(API_TEST_DIRECTORY, "test_cwl_conformance_%s.py" % version_simple), "w") as f:
         f.write(test_file_contents)
+    with open(os.path.join(API_TEST_DIRECTORY, "test_cwl_conformance_green_%s.py" % version_simple), "w") as f:
+        f.write(green_test_file_contents)
 
 
 if __name__ == "__main__":
