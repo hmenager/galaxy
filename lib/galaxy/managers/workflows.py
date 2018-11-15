bd8db3b3561cc48686b7c8fa8a89d2a6df80346a
John Chilton <jmchilton@gmail.com>
2018-11-15 11:33:24 +0100

    Dynamically load dynamic tools for workflows.

--- lib/galaxy/managers/workflows.py
+++ lib/galaxy/managers/workflows.py
@@ -579,7 +579,7 @@ class WorkflowContentsManager(UsesAnnotations):
             step_model = None
             if step.type == 'tool':
                 incoming = {}
-                tool = trans.app.toolbox.get_tool(step.tool_id, tool_version=step.tool_version)
+                tool = trans.app.toolbox.get_tool(step.tool_id, tool_version=step.tool_version, tool_hash=step.tool_hash)
                 params_to_incoming(incoming, tool.inputs, step.state.inputs, trans.app)
                 step_model = tool.to_json(trans, incoming, workflow_building_mode=workflow_building_modes.USE_HISTORY)
                 step_model['post_job_actions'] = [{
